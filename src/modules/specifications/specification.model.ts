cc9377e4f958c11ae1f9d500777ddd3dd080bb36
Renato Silva <renatosilvacode@gmail.com>
2023-10-14 18:14:00 -0300

    feat: create specification model

---
+++ src/modules/specifications/specification.model.ts
@@ -0,0 +1,16 @@
+import { randomUUID } from 'node:crypto'
+
+export class Specification {
+  id?: string
+  name: string
+  description: string
+  created_at?: Date
+
+  constructor() {
+    if (!this.id) {
+      this.id = randomUUID()
+    }
+
+    this.created_at = new Date()
+  }
+}
